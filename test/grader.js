ce64e0f662e863e2a9cdbdeb397e6ba6f3a76bd4
Vijaya Karumudi <vijaya.karumudi@yahoo.com>
2013-07-17 05:13:31 +0000

    Updated grader.js

--- test/grader.js
+++ test/grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var URL_DEFAULT  = "http://agile-refuge-5608.herokuapp.com/";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +39,34 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+var assertUrlExists = function() {
+     rest.get(program.url).on('complete', function(result) {
+     fs.writeFile('index1.html', result);
+     htmlfile = 'index1.html';
+     });
+     
+};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+    
+    if(!program.file){
+     rest.get(program.url).on('complete', function(result) {
+     fs.writeFile('index1.html', result);
+     htmlfile = 'index1.html';
+     });
+    }
     $ = cheerioHtmlFile(htmlfile);
+    
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,6 +86,7 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <input_url>', 'URL path')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
